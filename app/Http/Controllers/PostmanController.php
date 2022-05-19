dd8c8322d36f1a64eb32abf4ba02539332294fb5
Gerson <ondangangerson@gmail.com>
2022-05-19 08:06:35 +0800

    api postman

---
+++ app/Http/Controllers/PostmanController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostmanController extends Controller
+{
+    public function postapi()
+    {
+        return 'this is my new api course';
+    }
+}
