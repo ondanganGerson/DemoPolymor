9a0e77553ea341be9e42490528873b1a9fd99bc8
Gerson <ondangangerson@gmail.com>
2023-01-11 07:13:09 +0800

    creating test

--- routes/api.php
+++ routes/api.php
@@ -23,23 +23,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//get all room table
+//for postman test 
 Route::get('/room',[PostmanController::class, 'getRoom'])->name('room');
-//get all room table by id
 Route::get('/roomById/{id}',[PostmanController::class, 'getRoomById'])->name('roomById');
-//add new room table 
 Route::post('/addRoom',[PostmanController::class, 'addNewRoom'])->name('addRoom'); 
-//update room
 Route::put('/updateRooms/{id}',[PostmanController::class, 'updateRoom'])->name('updateRooms'); 
-//delete room
 Route::delete('/deleteRoom/{id}',[PostmanController::class, 'RemoveRoom'])->name('deleteRoom');
 
 
 // Route::apiResource('/author',AuthorController::class);
-Route::get('/author', [ApiControllerAuthorController::class, 'index'])->name('author'); //all
-Route::get('showAuthor/{id}', [ApiControllerAuthorController::class, 'show'])->name('showAuthor');  //show by id
-Route::post('/storeAuthor', [ApiControllerAuthorController::class, 'store'])->name('storeAuthor');  //store
-Route::put('updateAuthor/{id}', [ApiControllerAuthorController::class, 'update'])->name('updateAuthor'); //
+Route::get('/author', [ApiControllerAuthorController::class, 'index'])->name('author'); 
+Route::get('showAuthor/{id}', [ApiControllerAuthorController::class, 'show'])->name('showAuthor');  
+Route::post('/storeAuthor', [ApiControllerAuthorController::class, 'store'])->name('storeAuthor'); 
+Route::put('updateAuthor/{id}', [ApiControllerAuthorController::class, 'update'])->name('updateAuthor'); 
 Route::delete('/deleteAuthor/{id}', [ApiControllerAuthorController::class, 'destroy'])->name('deleteAuthor');
 
 
